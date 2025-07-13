1cdd4bf24acb91a4e289b6a7ba3b4e914a520f5e
Samarthanand Kamble <samarthanand.kamble@webileapps.com>
2025-07-13 15:21:31 +0530

    add user and authentication routes to app

--- src/app.js
+++ src/app.js
@@ -1,20 +1,43 @@
 const express = require("express");
 require('dotenv').config();
 
-
-console.log("express version:", express);
-
 const app = express();
 
 const port = process.env.PORT;
 
-app.use("/", (req, res) => {
-  res.send("Hello World!");
+
+app.get("/user", (req, res) => {
+  res.send("This is a GET users route!");
 })
 
-app.use("/user", (req, res) => {
-  res.send("This is users route!");
+app.post("/user", (req, res) => {
+  res.send("This is a POST users route!");
 })
+
+app.put("/user", (req, res) => { 
+  res.send("This is a PUT users route!");
+})
+
+app.delete("/user", (req, res) => {
+  res.send("This is a DELETE users route!");
+})
+
+app.get("/logout", (req, res) => {
+  res.redirect(200,"/login");
+  // res.json({
+  //   message: "This is a GET logout route!",
+  //   status: "success"
+  // });
+})
+
+app.get("/login", (req, res) => {
+  res.json({
+    message: "This is a GET login route!",
+    status: "success"
+  });
+})
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
