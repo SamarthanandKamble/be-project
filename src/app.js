4af0ecc6e319188a5ee053b66e4eacc97e0b0ac2
Samarthanand Kamble <samarthanand.kamble@webileapps.com>
2025-07-13 17:31:22 +0530

    refactor logout route and add new route handlers for hello and abc endpoints

--- src/app.js
+++ src/app.js
@@ -22,14 +22,6 @@ app.delete("/user", (req, res) => {
   res.send("This is a DELETE users route!");
 })
 
-app.get("/logout", (req, res) => {
-  res.redirect(200,"/login");
-  // res.json({
-  //   message: "This is a GET logout route!",
-  //   status: "success"
-  // });
-})
-
 app.get("/login", (req, res) => {
   res.json({
     message: "This is a GET login route!",
@@ -37,6 +29,42 @@ app.get("/login", (req, res) => {
   });
 })
 
+app.get("/logout", (req, res) => {
+  // res.redirect(200,"/login");
+  res.json({
+    message: "This is a GET logout route!",
+    status: "success"
+  });
+})
+
+// playing with the route handlers
+// first the req handlers will execute and then it will execute the final route handler.
+// if we dont use next() in the req handlers, the final route handler will not execute.
+const handler1 = (req,res,next) => {
+  console.log("Handler 1 executed"); 
+  next();
+}
+
+const handler2 = (req,res,next) => {
+  console.log("Handler 2 executed");
+  next();
+}
+
+app.get("/hello", handler1, handler2, (req, res) => {
+  res.send("Hello World!");
+})
+
+
+app.get("/abc1", (req, res,next) => {
+
+  next()
+})
+app.get("/abc2", (req, res,next) => {
+  res.send("This is a GET abc2 route!");
+})
+app.get("/abc3", (req, res,next) => {
+  res.send("This is a GET abc3 route!");
+})
 
 
 app.listen(port, () => {
