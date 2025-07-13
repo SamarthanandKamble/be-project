d2389c142cbec6ba675806b49b015230e929fca0
Samarthanand Kamble <samarthanand.kamble@webileapps.com>
2025-07-13 13:21:26 +0530

    add user route to app

--- src/app.js
+++ src/app.js
@@ -12,6 +12,10 @@ app.use("/", (req, res) => {
   res.send("Hello World!");
 })
 
+app.use("/user", (req, res) => {
+  res.send("This is users route!");
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
