96ae029ab7340dce7965c695764353a801256341
Samarthanand Kamble <samarthanandkamble1002@gmail.com>
2025-08-31 20:48:02 +0530

    add validator package, and added validation

--- src/model/user.js
+++ src/model/user.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
+var validator = require('validator');
 
 const userSchema = new mongoose.Schema({
 firstName: { type: String, required: true, trim: true },
 lastName: { type: String, required: true},
-email: { type: String, required: true, unique: true },
-password: { type: String, required: true },
+email: { type: String, required: true, unique: [true,"Email already exists!"], validate:{
+    validator:(email)=> validator.isEmail(email),
+    message: 'Invalid email format'
+}},
+    password: {
+        type: String, required: true, validate: {
+    validator:(password)=> validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1
+    }),
+    message: 'Password is not strong enough'
+} },
 city: { type: String },
 }, {
     timestamps:true
