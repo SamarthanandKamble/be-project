4e048a7ef00e015307088139117928fb924c7b3c
Samarthanand Kamble <samarthanandkamble1002@gmail.com>
2025-09-06 21:40:53 +0530

    added endpoints separate file

---
+++ src/utils/endpoints.js
@@ -0,0 +1,20 @@
+const endpoints = {
+    AUTH: {
+        LOGIN: "/login",
+        SIGNUP: "/signup",
+        FORGOT_PASSWORD: "/forgotPassword",
+        UNLOCK_ACCOUNT: "/unlockAccount",
+    },
+    PROFILE: {
+        PROFILE: "/profile",
+    },
+    DASHBOARD: {
+        /*
+        getStats
+        getNotifications
+        markNotificationRead
+        */
+    },
+}
+
+module.exports = endpoints;
